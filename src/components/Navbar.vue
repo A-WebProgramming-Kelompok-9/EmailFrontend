8c77aaeccd6dc3c6765f47daeaf58d38d72c8455
yuriko192 <ivanwijaya19@yahoo.co.id>
2020-10-26 22:18:43 +0700

    init

---
+++ src/components/Navbar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="p-2 navibar">
+    <a class="navbar-brand" href="#"> Speedwagon Mail</a>
+    <div class="ml-auto">
+      <a href="" class="btn btn-dark mr-2">Login</a>
+      <a href="" class="btn btn-outline-light mr-2">Register</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+name: "Navbar"
+}
+</script>
+
+<style scoped lang="scss">
+.navibar{
+  display: flex;
+  color: white;
+  width: 100vw;
+  z-index: 99;
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+*{
+  color: inherit;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,48 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="index">
+    <div class="body-container">
+      <navbar></navbar>
+      <land></land>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import navbar from "@/components/Navbar"
+import land from "@/components/Home/Land"
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
+    navbar,
+    land
   }
 }
 </script>
+
+<style scoped lang="scss">
+.index{
+  background: linear-gradient(to bottom, rgba(0, 0, 0, 0) 0%, $brown-200 90%),
+  url(../assets/Speedwagon_sepia.jpg) no-repeat $brown-200;
+  background-position: 0px -130px;
+  background-size: 100vw 100vh;
+  &::-webkit-scrollbar {
+    width: 10px;
+  }
+
+  &::-webkit-scrollbar-track {
+    box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: rgba(0, 0, 0, 0.288);
+    outline: 1px solid slategrey;
+    border-radius: 50px;
+  }
+  .body-container{
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+}
+</style>
