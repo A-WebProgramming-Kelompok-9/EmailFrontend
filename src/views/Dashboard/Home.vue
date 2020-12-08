87de7f298267d70fcab245013085695dc907a10a
yuriko192 <ivanwijaya19@yahoo.co.id>
2020-12-08 20:38:17 +0700

    added infinite loading to dashboard

--- src/views/Dashboard/Home.vue
+++ src/views/Dashboard/Home.vue
@@ -1,17 +1,41 @@
 <template>
   <div class="home">
     <searchbar></searchbar>
-    Home
+    <div v-for="i in data" :key="i.name">
+      {{i.name}}
+    </div>
+    <infinite-loading @infinite="loadMail">
+      <div slot="no-more">Loaded All Messages</div>
+      <div slot="no-results">There are no Email</div>
+    </infinite-loading>
   </div>
 </template>
 
 <script>
 import searchbar from "@/components/Dashboard/Search"
+import InfiniteLoading from 'vue-infinite-loading';
 
 export default {
   name: "Home",
   components: {
-    searchbar
+    searchbar,
+    InfiniteLoading
+  },
+  data() {
+    return {
+      data: [],
+      busy: false,
+      count:0
+    }
+  },
+  methods: {
+    loadMail($state) {
+      this.data.push({ name: this.count++ });
+      $state.loaded()
+      if(this.count>100){
+        $state.complete()
+      }
+    }
   },
 
 }
