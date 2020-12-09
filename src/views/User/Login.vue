f9cf0d999cf82f52644587a1673c429100ae4834
Natanael535180006 <natanael.535180006@stu.untar.ac.id>
2020-12-09 09:36:08 +0700

    router replace added

--- src/views/User/Login.vue
+++ src/views/User/Login.vue
@@ -75,6 +75,7 @@ export default {
       ).then(result => {
         if(result.status == "OK"){
           localStorage.user = result.content;
+          this.$router.replace("/views/Dashboard")
         }else{
           console.log(result)
         }
